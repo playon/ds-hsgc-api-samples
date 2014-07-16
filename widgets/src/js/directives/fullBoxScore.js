d2ee082fb2fb4ddf891b44f9fda3b8d6d6b53369
Luke Hertert <luke.hertert@gmail.com>
2014-07-16 13:37:42 -0400

    Adding sample buttons

--- widgets/src/js/directives/fullBoxScore.js
+++ widgets/src/js/directives/fullBoxScore.js
@@ -3,7 +3,26 @@ angular.module('hsgc')
     return {
       restrict: 'AE',
       transclude: true,
-      scope: { unityGameKey: "@game", publisherKey: "@publisher" },
+      scope: {
+        unityGameKey: "@game",
+        publisherKey: "@publisher"
+      },
+      controller: ['$scope', function($scope) {
+        $scope.getSubscribeUrl = function(dataOnly) {
+          var base = '/subscribe/';
+
+          var qs = '?' +
+            'publisher_key=' + $scope.publisherKey +
+            '&game_key=' + $scope.unityGameKey +
+            '&referrer=' + encodeURIComponent(window.location.href);
+
+          var subscriptionType = 'event';
+          if (dataOnly) {
+            subscriptionType = 'stats';
+          }
+          return base + subscriptionType + qs;
+        };
+      }],
       templateUrl: 'templates/fullBoxScore.html',
       replace: true
     };
