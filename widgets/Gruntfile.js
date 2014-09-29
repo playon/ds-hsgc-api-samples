8f3e53dff2c456850640180b354cc15b7ec7eba4
Chris Chandler <chris.chandler@digitalscout.com>
2014-09-29 16:40:25 -0400

    added angular as a built in concated dependency

--- widgets/Gruntfile.js
+++ widgets/Gruntfile.js
@@ -24,6 +24,7 @@ module.exports = function (grunt) {
                 },
                 files: {
                     'build/hsgc-widgets.min.js': [
+                      'src/js/vendor/angular.js',
                       'src/js/app.js',
                       tmpTemplateFile.path,
                       'src/js/*/*.js'
