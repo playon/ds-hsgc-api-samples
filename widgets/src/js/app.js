592dc5cf77d3e99d86408b291899309aa192ff3b
Luke Hertert <luke.hertert@gmail.com>
2014-10-08 17:38:00 -0400

    Add helper for network authorize

--- widgets/src/js/app.js
+++ widgets/src/js/app.js
@@ -36,3 +36,24 @@ hsgcWidgets = {
     angular.bootstrap(document, ['hsgc']);
   }
 };
+
+hsgcWidgets.networkAuthorize = function(gameKey, publisherKey, cb) {
+  nfhs.auth.datacast(gameKey, publisherKey, function(auth) {
+    if (auth.authorized) {
+      cb();
+    } else {
+      if (!hsgcWidgets.upsold) {
+        hsgcWidgets.upsold = true;
+        nfhs.utils.getUnityObject('game', gameKey, function (game) {
+          nfhs.templater.datacastUpsell(game, function (upsellTemplateVars) {
+            if (typeof(upsellTemplateVars != "undefined") && upsellTemplateVars) {
+              var html = nfhsplayer.templates.upsell(upsellTemplateVars);
+              html = html + "<p></p>";
+              $('.eventdetails').before($(html));
+            }
+          });
+        });
+      }
+    }
+  });
+};
