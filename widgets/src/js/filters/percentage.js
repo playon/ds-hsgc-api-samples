d9dc0cdc4ae40f0f7f7b09cfdbb4292d414ccf60
Luke Hertert <luke.hertert@gmail.com>
2014-11-10 18:10:17 -0500

    Add percentages to team stats

---
+++ widgets/src/js/filters/percentage.js
@@ -0,0 +1,13 @@
+angular.module('hsgc')
+  .filter('percentage', function ($window) {
+      return function (input, decimals, prefix, suffix) {
+          decimals = angular.isNumber(decimals)? decimals :  3;
+          suffix = suffix || '%';
+          prefix = prefix || '';
+
+          if ($window.isNaN(input) || !$window.isFinite(input)) {
+              return '';
+          }
+          return prefix + Math.round(input * Math.pow(10, decimals + 2))/Math.pow(10, decimals) + suffix
+      };
+  });
