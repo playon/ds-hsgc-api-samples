0df622a922a8b91dbb0e61c9221024ae564966a1
Jon Adams <jadams@firetiger.net>
2017-01-25 10:21:26 -0500

    widgets: fix full box score to always refresh regularly even after an API error reponse

--- widgets/src/js/directives/gameSummary.js
+++ widgets/src/js/directives/gameSummary.js
@@ -13,8 +13,12 @@ angular.module('hsgc')
           HSGCApi.getFullBox($scope.gameKey, $scope.publisherKey, $scope.sport, {
             includeTeamAggregates: true
           }).then(function(result) {
+            // success
             angular.extend($scope, result);
             $timeout(updateBoxScore, 30 * 1000);
+          }, function(result) {
+            // error
+            $timeout(updateBoxScore, 60 * 1000);
           });
         };
         config.beforeLoadDatacast($scope.gameKey, $scope.publisherKey, function() {
