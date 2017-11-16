21b69208c79a0039364b4ca1da8889293a6c4e92
Jon Adams <jadams@firetiger.net>
2017-11-16 13:08:57 -0500

    Widgets: Reduced 'football drive chart' memory usage

--- widgets/src/js/directives/footballDriveChart.js
+++ widgets/src/js/directives/footballDriveChart.js
@@ -9,7 +9,13 @@ angular.module('hsgc')
         var fontYards = "Open Sans, sans-serif";
         var yardLineWidth = 3;
         var maxEndzoneTextCharacters = 22;
-        var scrimmageLineColor = "#ffff00";
+        var hashMarkHeight = 10;
+        var yardNumberColor = new window.paper.Color(255, 255, 255); // #fff
+        var yardLineColor = yardNumberColor;
+        var scrimmageLineColor = new window.paper.Color(255, 255, 0);
+        var firstDownLineColor = new window.paper.Color(0, 255, 255);
+        var endZoneAwayColor = new window.paper.Color(255, 0, 0);
+        var endZoneHomeColor = new window.paper.Color(0, 0, 255);
 
         // internal tracking variables
         scope.selectedPeriod = 0;
@@ -114,28 +120,28 @@ angular.module('hsgc')
             yardLines.push(endzoneWidth + i * playingFieldWidth / 100);
           }
           var awayEndzone = new paper.Path.Rectangle(new paper.Rectangle(0, 0, endzoneWidth, fieldSize.height));
-          awayEndzone.fillColor = "#ff0000";
+          awayEndzone.fillColor = endZoneAwayColor;
 
           var endZoneTextHeight = endzoneWidth / 3;
           var awayTeamNameToUse = scope.awayAcronym && scope.awayAcronym.length > 1 && scope.awayShortName.length >= maxEndzoneTextCharacters ? scope.awayAcronym : scope.awayShortName;
           var awayTeamNameSize = scope.getTextSize(awayTeamNameToUse, endZoneTextHeight, fontTeams);
           var awayTeamName = new paper.PointText(new paper.Point( (endzoneWidth / 2) - (awayTeamNameSize.width / 2), (fieldSize.height / 2) + (awayTeamNameSize.height / 2)));
           awayTeamName.content = awayTeamNameToUse;
-          awayTeamName.strokeColor = "#ffffff";
-          awayTeamName.fillColor = "#ffffff";
+          awayTeamName.strokeColor = yardNumberColor;
+          awayTeamName.fillColor = yardNumberColor;
           awayTeamName.fontSize = awayTeamNameSize.height + "px";
           awayTeamName.fontFamily = fontTeams;
           awayTeamName.rotate(270);
 
           var homeEndzone = new paper.Path.Rectangle(new paper.Rectangle(yardLines[yardLines.length - 1], 0, endzoneWidth, fieldSize.height));
-          homeEndzone.fillColor = "#0000ff";
+          homeEndzone.fillColor = endZoneHomeColor;
 
           var homeTeamNameToUse = scope.homeAcronym && scope.homeAcronym.length > 1 && scope.homeShortName.length >= maxEndzoneTextCharacters ? scope.homeAcronym : scope.homeShortName;
           var homeTeamNameSize = scope.getTextSize(homeTeamNameToUse, endZoneTextHeight, fontTeams);
           var homeTeamName = new paper.PointText(new paper.Point(yardLines[yardLines.length - 1] + (endzoneWidth / 2) - (homeTeamNameSize.width / 2), (fieldSize.height / 2) + (homeTeamNameSize.height / 2)));
           homeTeamName.content = homeTeamNameToUse;
-          homeTeamName.strokeColor = "#ffffff";
-          homeTeamName.fillColor = "#ffffff";
+          homeTeamName.strokeColor = yardNumberColor;
+          homeTeamName.fillColor = yardNumberColor;
           homeTeamName.fontSize = awayTeamNameSize.height + "px";
           homeTeamName.fontFamily = fontTeams;
           homeTeamName.rotate(90);
@@ -148,20 +154,36 @@ angular.module('hsgc')
           }
 
           var yardLineTextVerticalPadding = textHeight * 0.1;
-          var yardLineTextHorizontalPadding = 1;
+          var yardLineTextHorizontalPadding = 2;
           var yardLineSpacing = yardLines[1] - yardLines[0];
           var hashMarkInset = fieldSize.height / 3;
           var topMiddleHashMarkY = hashMarkInset;
           var bottomMiddleHashMarkY = fieldSize.height - hashMarkInset;
+
+          // build reusable symbols
+          var hashMarkSymbol = new paper.Path.Line(new paper.Point(0, 0), new paper.Point(0, hashMarkHeight));
+          hashMarkSymbol.strokeColor = yardLineColor;
+          hashMarkSymbol.pivot = hashMarkSymbol.bounds.topLeft;
+          hashMarkSymbol = new paper.Symbol(hashMarkSymbol);
+          var verticalLineSymbol = new paper.Path.Line(new paper.Point(0, 0), new paper.Point(0, fieldSize.height));
+          verticalLineSymbol.strokeColor = yardNumberColor;          
+          verticalLineSymbol.strokeWidth = 1;
+          verticalLineSymbol.pivot = verticalLineSymbol.bounds.topLeft;
+          verticalLineSymbol = new paper.Symbol(verticalLineSymbol);
+          var thickVerticalLineSymbol = new paper.Path.Line(new paper.Point(0, 0), new paper.Point(0, fieldSize.height));
+          thickVerticalLineSymbol.strokeColor = yardNumberColor;
+          thickVerticalLineSymbol.strokeWidth = yardLineWidth;          
+          thickVerticalLineSymbol.pivot = thickVerticalLineSymbol.bounds.topLeft;
+          thickVerticalLineSymbol = new paper.Symbol(thickVerticalLineSymbol);
           
           for (i = 0; i < yardLines.length; yardLineSpacing > 5 ? i++ : i += 5) {
             // every 5 yards
             if (i % 5 === 0) {
-              var line = new paper.Path.Line(new paper.Point(yardLines[i], 0), new paper.Point(yardLines[i], fieldSize.height));
-              line.strokeColor = "#ffffff";
-              if (i % 10 === 0) {
+              if (i % 10 !== 0) {
+                verticalLineSymbol.place(new paper.Point(yardLines[i], 0));
+              } else if (i % 10 === 0) {
                 // every 10 yards
-                line.strokeWidth = yardLineWidth;
+                thickVerticalLineSymbol.place(new paper.Point(yardLines[i], 0));
                 if (i !== 0 && i !== yardLines.length - 1) {
                   var yardLine = i;
                   if (yardLine > 50) {
@@ -169,46 +191,46 @@ angular.module('hsgc')
                   }
                   yardLine = yardLine / 10;
                   var text10DigitSize = textSizes[yardLine];
-                  var topYardText10Digit = new paper.PointText(new paper.Point(yardLines[i] + yardLineTextHorizontalPadding + yardLineWidth - 1, text10DigitSize.height + yardLineTextVerticalPadding));
+                  var topYardText10Digit = new paper.PointText(new paper.Point(yardLines[i] + yardLineTextHorizontalPadding + yardLineWidth - 2, text10DigitSize.height + yardLineTextVerticalPadding));
                   topYardText10Digit.content = yardLine;
-                  topYardText10Digit.strokeColor = "#ffffff";
-                  topYardText10Digit.fillColor = "#ffffff";
+                  topYardText10Digit.strokeColor = yardNumberColor;
+                  topYardText10Digit.fillColor = yardNumberColor;
                   topYardText10Digit.fontSize = text10DigitSize.height + "px";
                   topYardText10Digit.fontFamily = fontYards;
                   topYardText10Digit.rotate(180);
 
                   var yardText0DigitSize = textSizes[0];
-                  var topYardText0Digit = new paper.PointText(new paper.Point(yardLines[i] - text10DigitSize.width - yardLineTextHorizontalPadding, yardText0DigitSize.height + yardLineTextVerticalPadding));
+                  var topYardText0Digit = new paper.PointText(new paper.Point(yardLines[i] - text10DigitSize.width - yardLineTextHorizontalPadding - 2, yardText0DigitSize.height + yardLineTextVerticalPadding));
                   topYardText0Digit.content = 0;
-                  topYardText0Digit.strokeColor = "#ffffff";
-                  topYardText0Digit.fillColor = "#ffffff";
+                  topYardText0Digit.strokeColor = yardNumberColor;
+                  topYardText0Digit.fillColor = yardNumberColor;
                   topYardText0Digit.fontSize = yardText0DigitSize.height + "px";
                   topYardText0Digit.fontFamily = fontYards;
                   topYardText0Digit.rotate(180);
 
                   text10DigitSize = textSizes[yardLine];
-                  var bottomYardText10Digit = new paper.PointText(new paper.Point(yardLines[i] - text10DigitSize.width - yardLineTextHorizontalPadding, fieldSize.height - text10DigitSize.height / 2 - yardLineTextVerticalPadding));
+                  var bottomYardText10Digit = new paper.PointText(new paper.Point(yardLines[i] - text10DigitSize.width - yardLineTextHorizontalPadding - 1, fieldSize.height - text10DigitSize.height / 2 - yardLineTextVerticalPadding));
                   bottomYardText10Digit.content = yardLine;
-                  bottomYardText10Digit.strokeColor = "#ffffff";
-                  bottomYardText10Digit.fillColor = "#ffffff";
+                  bottomYardText10Digit.strokeColor = yardNumberColor;
+                  bottomYardText10Digit.fillColor = yardNumberColor;
                   bottomYardText10Digit.fontSize = text10DigitSize.height + "px";
                   bottomYardText10Digit.fontFamily = fontYards;
 
                   yardText0DigitSize = textSizes[0];
-                  var bottomYardText0Digit = new paper.PointText(new paper.Point(yardLines[i] + yardLineTextHorizontalPadding + yardLineWidth, fieldSize.height - yardText0DigitSize.height / 2 - yardLineTextVerticalPadding));
+                  var bottomYardText0Digit = new paper.PointText(new paper.Point(yardLines[i] + yardLineTextHorizontalPadding + yardLineWidth - 1, fieldSize.height - yardText0DigitSize.height / 2 - yardLineTextVerticalPadding));
                   bottomYardText0Digit.content = 0;
-                  bottomYardText0Digit.strokeColor = "#ffffff";
-                  bottomYardText0Digit.fillColor = "#ffffff";
+                  bottomYardText0Digit.strokeColor = yardNumberColor;
+                  bottomYardText0Digit.fillColor = yardNumberColor;
                   bottomYardText0Digit.fontSize = yardText0DigitSize.height + "px";
                   bottomYardText0Digit.fontFamily = fontYards;
                 }
               }
             } else if (yardLineSpacing > 5) {
               // loops every yard, but only if there is enough space for individual yard hashes
-              scope.drawHashMark(yardLines[i], 0, true);
-              scope.drawHashMark(yardLines[i], fieldSize.height, false);
-              scope.drawHashMark(yardLines[i], topMiddleHashMarkY, true);
-              scope.drawHashMark(yardLines[i], bottomMiddleHashMarkY, false);
+              scope.drawHashMark(hashMarkSymbol, yardLines[i], 0, true);
+              scope.drawHashMark(hashMarkSymbol, yardLines[i], fieldSize.height, false);
+              scope.drawHashMark(hashMarkSymbol, yardLines[i], topMiddleHashMarkY, true);
+              scope.drawHashMark(hashMarkSymbol, yardLines[i], bottomMiddleHashMarkY, false);
             }
           }
         };
@@ -251,7 +273,7 @@ angular.module('hsgc')
 
           var firstDownLineX = yardLines[firstDownYardLine];
           var firstDownLine = new paper.Path.Line(new paper.Point(firstDownLineX, 0), new paper.Point(firstDownLineX, fieldSize.height));
-          firstDownLine.strokeColor = "#00ffff";
+          firstDownLine.strokeColor = firstDownLineColor;
           firstDownLine.strokeWidth = 5;
 
           paperDriveGroup = new paper.Group([scrimmageLine, arrow, firstDownLine]);
@@ -259,11 +281,8 @@ angular.module('hsgc')
           paper.view.draw();
         };
 
-        scope.drawHashMark = function(x, y, drawTopToBottom) {
-          var hashMarkHeight = 10;
-          var yCoord = drawTopToBottom ? y : y - hashMarkHeight;
-          var mark = new paper.Path.Line(new paper.Point(x, yCoord), new paper.Point(x, yCoord + hashMarkHeight));
-          mark.strokeColor = "#ffffff";
+        scope.drawHashMark = function(symbol, x, y, drawTopToBottom) {
+          symbol.place(new paper.Point(x, drawTopToBottom ? y : y - hashMarkHeight));
         };
 
         scope.getTextSize = function(text, height, font) {
